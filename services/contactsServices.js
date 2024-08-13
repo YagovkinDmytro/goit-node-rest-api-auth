241b2042cd3965badd3b264b8f9d820d1bafe5a6
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-08-13 10:50:46 +0200

    add pagination contactsControllers.js

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,9 +1,15 @@
 import Contact from "../db/models/Contact.js";
 
-export const getContacts = (query) =>
-  Contact.findAll({
+export const getContacts = (query, { page = 1, limit = 20 }) => {
+  const normalizedLimit = Number(limit);
+  const offset = (Number(page) - 1) * normalizedLimit;
+
+  return Contact.findAll({
     where: query,
+    offset: offset,
+    limit: normalizedLimit,
   });
+};
 
 export const getContact = (query) =>
   Contact.findOne({
